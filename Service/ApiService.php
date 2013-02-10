a3c7c6c522ca505fd731c4b463474aa01c720653
Robert Elwell <robert@wikia-inc.com>
2013-02-10 14:08:25 -0800

    introduce first service method

--- Service/ApiService.php
+++ Service/ApiService.php
@@ -11,14 +11,36 @@ use \zendesk;
  */
 class ApiService
 {
-	/**
-	 * Interface to core API request wrapper
-	 * @var \zendesk
-	 */
-	protected $api;
-	
-	public function __construct( $apiKey, $user, $subDomain )
-	{
-		$this->api = new zendesk( $apiKey, $user, $subDomain );
-	}
+    /**
+     * Interface to core API request wrapper
+     * @var \zendesk
+     */
+    protected $api;
+    
+    /**
+     * Creates API wrapper 
+     * @param string $apiKey
+     * @param string $user
+     * @param string $subDomain
+     */
+    public function __construct( $apiKey, $user, $subDomain )
+    {
+        $this->api = new zendesk( $apiKey, $user, $subDomain );
+    }
+    
+    /**
+     * Sends a create request to API's user service
+     * Returns a json-decoded array from the response
+     * @param string $name
+     * @param string $email
+     * @return mixed return value of \zendesk::call
+     */
+    public function createSubmitter( $name, $email )
+    {
+        $jsonArray = array(
+                'name' => $name,
+                'email' => $email
+                );
+        $this->api->call( 'users' , json_encode( $jsonArray ), 'POST' );
+    } 
 }
