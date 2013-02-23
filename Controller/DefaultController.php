0ba43bd057a6f2614b4136d9dc7913493f13b0c7
Robert Elwell <robert@wikia-inc.com>
2013-02-22 17:39:58 -0800

    created a route for controller, but need to figure out how to make it work still

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -3,9 +3,18 @@
 namespace Malwarebytes\ZendeskBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Malwarebytes\ZendeskBundle\DataModel;
 
 class DefaultController extends Controller
 {
+    public function accessUserAction($name, $email)
+    {
+        $service = $this->get( 'api' );
+        $userRepo = new DataModel\User\Repository( $service );
+        $user = $userRepo->getForNameAndEmail($name, $email);
+        return print_r( $user->toArray() );
+    }
+    
     public function indexAction($name)
     {
         return $this->render('ZendeskBundle:Default:index.html.twig', array('name' => $name));
