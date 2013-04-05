e31b249d220f26c3e2cb2a20731f2e5c2eeb914c
Robert Elwell <robert@wikia-inc.com>
2013-04-04 19:01:12 -0700

    get current tests for service working

--- Tests/Service/ZendeskServiceTest.php
+++ Tests/Service/ZendeskServiceTest.php
@@ -8,18 +8,28 @@ class ZendeskServiceTest extends \PHPUnit_Framework_TestCase
 {
     /**
      * This gets depednency injected on construct so here's a helper method
-     * @param array|null $methods
      * @return Ambigous <PHPUnit_Framework_MockObject_MockObject, object>
      */
-    protected function _getRepoMock( $methods = null )
+    protected function _getRepoMock()
     {
-        return $this->getMockBuilder( 'Malwarebytes\ZendeskBundle\Service\RepositoryService' )
-                    ->disableOriginalConstructor()
+        $repo =  $this->getMockBuilder( 'Malwarebytes\ZendeskBundle\Service\RepositoryService' )
+                      ->disableOriginalConstructor()
+                      ->setMethods( array( 'get' ) )
+                      ->getMock();
+        $this->_repo = $repo;
+        return $repo;
+    }
+    
+    protected function _getServiceMock( $methods = null )
+    {
+        return $this->getMockBuilder( 'Malwarebytes\ZendeskBundle\Service\ZendeskService' )
+                    ->setConstructorArgs( array( $this->_getRepoMock() ) )
                     ->setMethods( $methods )
                     ->getMock();
     }
     
     /**
+     * @covers Malwarebytes\ZendeskBundle\Service\ZendeskService::__construct
      * @covers Malwarebytes\ZendeskBundle\Service\ZendeskService::getTicketsWithCommentsForUserId
      */
     public function testGetTicketsWithCommentsForUserId()
@@ -28,7 +38,85 @@ class ZendeskServiceTest extends \PHPUnit_Framework_TestCase
                      ->disableOriginalConstructor()
                      ->getMock();
         
+        $ticketRepo = $this->getMockBuilder( 'Malwarebytes\ZendeskBundle\DataModel\Ticket\Repository' )
+                           ->disableOriginalConstructor()
+                           ->setMethods( array( 'getTicketsRequestedByUser' ) )
+                           ->getMock();
         
+        $ticket = $this->getMockBuilder( 'Malwarebytes\ZendeskBundle\DataModel\Ticket\Entity' )
+                       ->disableOriginalConstructor()
+                       ->setMethods( array( 'offsetSet' ) )
+                       ->getMock();
+        
+        $service = $this->_getServiceMock( array( 'getTicketsRequestedByUser', 'getById', 'getCommentsForTicket' ) );
+        
+        $service
+            ->expects( $this->once() )
+            ->method ( 'getById' )
+            ->with   ( 'User', 123 )
+            ->will   ( $this->returnValue( $user ) )
+        ;
+        $this->_repo
+            ->expects( $this->once() )
+            ->method ( 'get' )
+            ->with   ( 'Ticket' )
+            ->will   ( $this->returnValue( $ticketRepo ) )
+        ;
+        $ticketRepo
+            ->expects( $this->once() )
+            ->method ( 'getTicketsRequestedByUser' )
+            ->with   ( $user )
+            ->will   ( $this->returnValue( array( $ticket ) ) )
+        ;
+        $service
+            ->expects( $this->once() )
+            ->method ( 'getCommentsForTicket' )
+            ->with   ( $ticket )
+            ->will   ( $this->returnValue( array( 'foo' ) ) )
+        ;
+        $ticket
+            ->expects( $this->once() )
+            ->method ( 'offsetSet' )
+            ->with   ( 'comments', array( 'foo' ) )
+        ;
+        $this->assertEquals(
+                array( $ticket ),
+                $service->getTicketsWithCommentsForUserId( 123 )
+        );
+    }
+    
+    /**
+     * @covers Malwarebytes\ZendeskBundle\Service\ZendeskService::getCommentsForTicket
+     */
+    public function testGetCommentsForTicket()
+    {
+        $auditRepo = $this->getMockBuilder( 'Malwarebytes\ZendeskBundle\DataModel\Audit\Repository' )
+                          ->disableOriginalConstructor()
+                          ->setMethods( array( 'getCommentsForTicket' ) )
+                          ->getMock();
+        
+        $ticket = $this->getMockBuilder( 'Malwarebytes\ZendeskBundle\DataModel\Ticket\Entity' )
+                       ->disableOriginalConstructor()
+                       ->setMethods( array( 'offsetSet' ) )
+                       ->getMock();
+        
+        $service = $this->_getServiceMock();
+        $this->_repo
+            ->expects( $this->once() )
+            ->method ( 'get' )
+            ->with   ( 'Audit' )
+            ->will   ( $this->returnValue( $auditRepo ) )
+        ;
+        $auditRepo
+            ->expects( $this->once() )
+            ->method ( 'getCommentsForTicket' )
+            ->with   ( $ticket )
+            ->will   ( $this->returnValue( array( 'foo' ) ) )
+        ;
+        $this->assertEquals(
+                array( 'foo' ),
+                $service->getCommentsForTicket( $ticket )
+        );
     }
     
 }
